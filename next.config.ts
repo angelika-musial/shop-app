051827d7a41846af506a1085aa55fa1782b12c38
Angelika Musiał <angelikamusial@tlen.pl>
2026-05-03 14:44:31 +0200

    chore: update next.config with specific hostnames for logo and product images

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,13 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: '**',
+				hostname: 'dummyimage.com',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+				pathname: '/**',
 			},
 		],
 	},
