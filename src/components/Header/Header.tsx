834c444a1cb6663be328992b5b21b7fa297da758
Angelika Musiał <angelikamusial@tlen.pl>
2026-05-03 17:57:30 +0200

    style: refactor styles using variables and polish product cards UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,25 +3,26 @@ import { CartBadge } from './CartBadge';
 import styles from './Header.module.scss';
 
 type Props = {
-    logo: { url: string; altText: string };
+	logo: { url: string; altText: string };
 };
 
 export const Header = ({ logo }: Props) => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <div className={styles.logoWrapper}>
-                    <Image
-                        src={logo.url}
-                        alt={logo.altText}
-                        width={200}
-                        height={60}
-                        priority
-                    />
-                </div>
+	return (
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.logoWrapper}>
+					<Image
+						src={logo.url}
+						alt={logo.altText}
+						width={160}
+						height={50}
+						priority
+						className={styles.logoImage}
+					/>
+				</div>
 
-                <CartBadge />
-            </div>
-        </header>
-    );
+				<CartBadge />
+			</div>
+		</header>
+	);
 };
